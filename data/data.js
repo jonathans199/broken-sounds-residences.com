2dc96f592c8ba1ab540eadbacc089ea007f3ea52
jonathans199 <jso1919@gmail.com>
2023-05-10 16:40:41 -0400

    home page layout

---
+++ data/data.js
@@ -0,0 +1,160 @@
+export const steps = [
+	{
+		id: 0,
+		title: 'Step 1',
+		subTitle: 'Make a call',
+		description:
+			'We’ll get to know each other and discuss your circumstances, concerns and dreams for a ideal purchase or sale.',
+		img: 'https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+	},
+	{
+		id: 1,
+		title: 'Step 2',
+		subTitle: 'PLAN FOR SUCCESS',
+		description:
+			'Next we will help you put a plan in place so you can achieve your goals quickly and without unnecessary stress',
+		img: 'https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+	},
+	{
+		id: 2,
+		title: 'Step 3',
+		subTitle: 'GET A DEAL YOU LOVE',
+		description: 'Nice and easy we will guide through a closing you love',
+		img: 'https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+	},
+	{
+		id: 3,
+		title: 'Step 4',
+		subTitle: 'GET LIFE LONG SUPPORT',
+		description:
+			"Our commitment doesn't end at closing, through our clients close relationship,  we will stay on your corner as long as you will have us. ",
+		img: 'https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+	},
+]
+
+export const sections = [
+	{
+		id: 0,
+		title: 'The old model of the country clubs does not work any more!',
+		order: 'lg:order-first',
+		description:
+			'Fortunately Broken Sound Country Club has proactively  transformed to meet modern real estate needs with refined luxury and a holistic lifestyle. Ideal for buyers and sellers seeking an elevated living experience.',
+		img: 'https://cdn0.weddingwire.com/vendor/460763/3_2/1280/jpg/bs-ca05-effectsresult-e-with-logo-1_51_367064-159906504173054.jpeg',
+	},
+	{
+		id: 1,
+		title: 'A new lifestyle beyond ',
+		order: 'lg:order-last',
+		description:
+			"Whatever one’s passion or interest...the Platinum Lifestyle  from healthy, business life, sporting life, and social life, complement each other perfectly at Broken Sound Club. Award winning, 5-Stars Platinum Club Located in Boca Raton,  is ready to welcome you home to the new era of Luxury.  The award-winning, private, gated golf and country club community offers a choice of 28 lushly landscaped intimate residential villages, each with its own character. South Florida's most exclusive and sought after private residential members club, Broken Sound Country Club dovetailing with the  multi-million dollars invested in 131,000 square feet outstanding Amenities and Facilities",
+		img: 'https://www.pearlantonacci.com/uploads/Broken-Sound-Rentals-Boca-Raton.png',
+	},
+	{
+		id: 2,
+		title: 'Live Where Golf Champions Play',
+		subTitle: 'Two is better than one. Golf Courses',
+		order: 'lg:order-first',
+		description:
+			'Old Course Home to the PGA Tour Champions Season Opener for 15 consecutive years The par 72  Old Course is a pure core golf course without housing. Lunch with friends overlooking the majestic 18th green! With the Own clubhouse with Dining Room, with both indoor and outdoor seating options, newly installed outdoor green-side seating overlooking the majestic 18th green. the course is the first golf facility in Florida, second in the U.S. and 14th in the world to be GEO Certified.',
+		img: 'https://i.ytimg.com/vi/2AU7knzMKJY/maxresdefault.jpg',
+	},
+	{
+		id: 3,
+		title: 'Club Course',
+		subTitle: 'More options on how to play each hole and suit your game.',
+		order: 'lg:order-last',
+		description:
+			"On-site Golf Academy with instructional resources including lessons available with PGA professionals, video system and clinics throughout the year. Full-service Golf Shop featuring the latest golf fashions and equipment, including custom fitting services. Specialized tournaments, such as “9 and Dine”, a 9-hole round followed by a special dining experience for couples, as well as Men's and Women's 18 and 9 hole leagues. Locally-renowned Member-Guest tournaments. Junior Golf Program featuring a fun learning environment and state-of-the-art instruction by PGA Golf Professionals. Sharpen your skills with the Club’s full staff of teaching pros.",
+		img: 'https://golfproperty.com/wp-content/uploads/2020/10/119b1396d8f2de0b4163561c289777bb-1.jpg',
+	},
+	{
+		id: 4,
+		title: 'Truly a tennis player’s paradise',
+		subTitle: 'Recognized as a USTA facility of the year',
+		order: 'lg:order-first',
+		description:
+			'Number 1 ranked Florida head pro offering both private and group instruction Come to enjoy one of our 23 Har Tru Tennis Courts, Broken Sound Club is a leader in tennis communities in south Florida and recognized as a USTA facility of the year.',
+		img: 'https://bocaratonobserver.com/downloads/6107/download/Broken-Tennis_EDIT_web.jpg?cb=5f0bbc79cbbea4228b664a1b05fde43c',
+	},
+	{
+		id: 5,
+		title: 'From the Michelin-starred environment ',
+		subTitle: 'With passion and attention to detail',
+		order: 'lg:order-last',
+		description:
+			'endless possibilities for you to have fun spending a pleasant time and enjoying with your friends after an after-golf tennis or pickle ball experience or just because you are not in a mood to cook you will have.  A breakfast, lunch or Dinner experience from the Michelin-starred environment with passion and attention to detail in 5 different environment according with your mood.',
+		img: 'https://brokensoundproperties.com/wp-content/uploads/2020/01/015-BrokenSound-BocaRaton-FL-72.jpg',
+	},
+	{
+		id: 6,
+		title: 'Cirque Bar and Grill',
+		subTitle: 'With passion and attention to detail',
+		order: 'lg:order-first',
+		description:
+			'Led by Executive Chef John Muriel, which will be open for lunch and dinner. It’s more of a casual, after-golf experience but will offer an elevated menu and transformed dining room at night. The Old Course Clubhouse Is our traditional dining space, is under the leadership of Executive Chef William Quick, who crafts elevated menus for our membership. The classic upscale menu and it will transition into a steakhouse on Saturdays.  We have table-side and cocktail carts run by our new Director of Bars, (Kord Laughlin).  Zest is our shareable, small-plates restaurant with sushi and tapas. It’s eclectic, and the menu will rotate.  Zest can also used as a pre-function space and reception area for our new ballroom, which holds up to 600 people.',
+		img: 'https://bocaratonobserver.com/downloads/6103/download/Broken-BS_grille003_e.jpg%20%281%29_web.jpg?cb=5f0bbc79cbbea4228b664a1b05fde43c&w=640',
+	},
+	{
+		id: 7,
+		title: 'Bistro',
+		subTitle: 'Moonstone cafe',
+		order: 'lg:order-last',
+		description:
+			'You are going to just fall in love with your mornings every day. Pop up at the moonstone cafe and enjoy besides the coffee, smoothies, snacks, salads, fresh fruits in your way to the golf courses o the tennis courts o just hang out with your computer and the Wi-Fi and enjoy like a starbucks experience.  Moonstone Cafe, Immerse in an ambience that provides relaxation and relieves stress you are going to just fall in love with your mornings every day with complimentary coffee, bagels, oatmeal, fresh fruits etc.',
+		img: 'https://clubandresortchef.com/wp-content/uploads/2022/09/Broken-Sound_restaurant-1024x576.jpg',
+	},
+	{
+		id: 8,
+		title: 'Moonstone Spa',
+		subTitle: 'In the middle of a hurried life, get away from the masses and connect with the spirit of loving.',
+		description:
+			'Broken Sound Spa  is about loving your self thought in every sense— and for sure — a renewed integration between heart, soul and body. Loving yourself is the way to reconnect generate health, fullness, and Rejuvenation in Broken sound 13,100 SqFt Moonstone Spa.  Designed in warm natural tones, the spa features six treatment rooms, a “Moonstone Suite” couples room, men’s and women’s locker rooms with showers, steam rooms, and whirlpools. Its Relaxation Rooms, one for men and one for women, offer the comfort of custom lounges, quiet lighting, and calming scents, as guests retreat and enjoy sipping on a hot tea, fruit water and other nutritious indulgences prior to a treatment or service.',
+		order: 'lg:order-first',
+		img: 'https://www.leisureopportunities.co.uk/images/655104_544800.jpg',
+	},
+	{
+		id: 9,
+		title: 'Moonstone Beauty Salon',
+		subTitle: 'Let the moonstone beauty salon take care of your beauty',
+		description:
+			'there is a 2,200-square-foot salon with stylist, two hair blow dry bars, two princess rooms for private pampering, and manicure and pedicure treatments, as well as a special “men’s only” spa treatment',
+		order: 'lg:order-last',
+		img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3p_NQBNnKBtpxVKK8nPBomNA1qLmiXbRTEiTO1udFp7lgVsIsLvT9r3a1pF9iVjxuRB0&usqp=CAU',
+	},
+	{
+		id: 10,
+		title: 'Moonstone Fitness Center',
+		subTitle: 'Get into de best shape of your life',
+		description:
+			'In the way searching for a healthy life the first steps are the hardest, At Broken Sound Fitness enjoy the three major benefits Motivation and Companionship Instruction and Supervision Equipment and facilities Get into de best shape of your life in the Broken sound 7,000-square-foot state-of-the-art fitness center offering the latest in fitness equipment, along with scheduled group classes in aerobics, Zumba, cardio kickboxing, tai chi, yoga, Pilates, sculpting, and children’s fitness classes in two exercise studios, and by-appointment private training sessions. The fitness center is equipped with 15 elliptical machines, 12 treadmills, 2 stair steppers, 11 recumbent bikes, a weights circuit of 20 machines, and TRX multi-mount monkey bars. Its dedicated Pilates area is furnished with six different types of equipment.',
+		order: 'lg:order-first',
+		img: 'https://brokensoundproperties.com/wp-content/uploads/2020/01/020-BrokenSound-BocaRaton-FL-72.jpg',
+	},
+	{
+		id: 11,
+		title: 'Moonstone Wellness Center',
+		subTitle: 'What’s get measured get managed',
+		description:
+			'A special 2,500-square-foot section of the fitness center is devoted to wellness services, in body assessment, in house nutritionist, offering convenient on-site physician consultation, physical therapy, and lecture rooms. “It’s really just another reason to come use the club,” here a 38,000 SqFt Moonstone Spa, cafe  Fitness Center and Wellness center, A designated Distinguished Emerald Club and one of “America’s Healthiest Clubs”',
+		order: 'lg:order-last',
+		img: 'https://scontent-mia3-2.xx.fbcdn.net/v/t1.6435-9/66354105_2744768498931813_3083242066998722560_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=7IAZbyfTcwkAX8Mwg4b&_nc_ht=scontent-mia3-2.xx&oh=00_AfAQdhzd_9uDcAklZdCNgAdcS7oEQlO8Dthmu5Qpz-ZjLw&oe=64836906',
+	},
+	{
+		id: 12,
+		title: 'Poolscape',
+		subTitle: 'Perfect place to practices your best swim style with no interruptions',
+		description:
+			'recently awarded by Club Resort and Business in the top best for Aquatics & Pools (2022) Two-acre poolscape and Bistro (Shake Shack style with drinks) overlooking the golf course and the kids playground area. recently awarded by Club Resort and Business in the top best for Aquatics & Pools (2022).  Adults pool with waterfalls, Hot Tub, Pool Laps, Bistro, kids pool with slides, Playground.  Perfect place to practices your best swim style with no interruptions or a warm deep at the adults pool with waterfalls, relaxing in the hot tub or having some drinks snacks or lunch poolside with the boca raton beautiful weather, also the kids enjoy no bothering you in the kids pool with slides, at the playground or in the dancing water area.  ',
+		order: 'lg:order-first',
+		img: 'https://www.brokensoundclub.org/images/dynamic/getImage.gif?ID=100017195',
+	},
+	{
+		id: 13,
+		title: 'Kids Corner',
+		subTitle: 'You can have the best possible start for your kids',
+		description:
+			'Broken Sound Club is surrounded by Boca Raton’s best schools, such as Calusa Elementary, Omni Middle School, and Spanish River High School. With the best schools around, and a community that fosters growth and healthy social interaction, Kids are using the club, loving the club, just really engaged! The new Broken sound Club brought more than 300 hundred new kids to the Club in the past two years no wonder why, FROM AFTER SCHOOL ACADEMIES TO SUMMER PROGRAMS FOR GOLF, TENNIS, FITNESS, BASKETBALL AND SWIM AMONG OTHERS, BROKEN SOUND WELCOMES FAMILIES WITH ACTIVITIES FOR ALL AGES. If you have a family or planning to have one, you can have the best possible start for your kids',
+		order: 'lg:order-last',
+		img: 'https://www.brokensoundclub.org/images/dynamic/getImage.gif?ID=100014005',
+	},
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,158 @@
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
 import Image from 'next/image'
 
+import { steps, sections } from '../../data/data'
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<>
+			<Header />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+			<section className='bg-gray-50 mb-40'>
+				<div className='mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center'>
+					<div className='mx-auto max-w-xl text-center'>
+						<h1 className='text-3xl font-extrabold sm:text-5xl'>
+							Find the home and the Holistic-Luxury
+							<strong className='font-extrabold text-emerald-600 sm:block'>
+								Lifestyle that you have been looking for{' '}
+							</strong>
+						</h1>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+						<p className='mt-4 sm:text-xl/relaxed'>
+							Our personalized approach ensures a seamless buying or selling process, helping you achieve top dollar for
+							your home with ease.
+						</p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+						<div className='mt-8 flex flex-wrap justify-center gap-4'>
+							<a
+								className='block w-full rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-emerald-700 focus:outline-none focus:ring active:bg-emerald-500 sm:w-auto'
+								href='/get-started'>
+								For Buyers
+							</a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+							<a
+								className='block w-full rounded px-12 py-3 text-sm font-medium text-emerald-600 shadow hover:text-emerald-700 focus:outline-none focus:ring active:text-emerald-500 sm:w-auto'
+								href='/about'>
+								For Sellers
+							</a>
+						</div>
+					</div>
+				</div>
+			</section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+			<main className='mx-auto max-w-screen-xl'>
+				<div className='mx-auto max-w-5xl px-4 py-8 my-40'>
+					<section className='rounded-lg bg-gray-100 p-8'>
+						<div className='grid grid-cols-1 gap-12 sm:grid-cols-3 sm:items-center'>
+							<img
+								alt='Man'
+								src='https://images.unsplash.com/photo-1625019030820-e4ed970a6c95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+								className='aspect-square w-full rounded-lg object-cover'
+							/>
+
+							<blockquote className='sm:col-span-2'>
+								<p className='text-xl font-medium sm:text-2xl'>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perspiciatis cumque neque ut nobis
+									excepturi, quasi iure quisquam autem alias.
+								</p>
+
+								<cite className='mt-8 inline-flex items-center not-italic'>
+									<span className='hidden h-px w-6 bg-gray-400 sm:inline-block'></span>
+									<p className='text-sm uppercase text-gray-500 sm:ms-3'>
+										<strong>Justin Harrison</strong>, Beatle Inc.
+									</p>
+								</cite>
+							</blockquote>
+						</div>
+					</section>
+				</div>
+
+				{sections.map(section => {
+					return (
+						<section key={section.id}>
+							<div className='mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-40 lg:px-8 '>
+								<div className='grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16'>
+									<div className={`relative h-64 overflow-hidden rounded-lg sm:h-80 ${section.order} lg:h-full`}>
+										<img alt='Party' src={section.img} className='absolute inset-0 h-full w-full object-cover' />
+									</div>
+
+									<div className='lg:py-24'>
+										<h2 className='text-3xl font-bold sm:text-4xl'>{section.title}</h2>
+										{section.subTitle && <h4 className='text-gray-600 sm:text-4xl'>{section.subTitle}</h4>}
+
+										<p className='mt-4 text-gray-600'>{section.description}</p>
+
+										<div className='mt-8 flex flex-wrap gap-4'>
+											<a
+												className='block w-full rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-emerald-700 focus:outline-none focus:ring active:bg-emerald-500 sm:w-auto'
+												href='/get-started'>
+												For Buyers
+											</a>
+
+											<a
+												className='block w-full rounded px-12 py-3 text-sm font-medium text-emerald-600 shadow hover:text-emerald-700 focus:outline-none focus:ring active:text-emerald-500 sm:w-auto'
+												href='/about'>
+												For Sellers
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<hr />
+						</section>
+					)
+				})}
+
+				<iframe
+					src='https://www.google.com/maps/embed?pb=!4v1683679498772!6m8!1m7!1sCAoSLEFGMVFpcE41cER6YUd4SnI5RWJ2Yk5pSFV1b2tRN2VzLTJzVjNqVmNTYXZB!2m2!1d26.39548429891705!2d-80.13625969999998!3f7.150000095367432!4f0!5f0.7820865974627469'
+					width='100%'
+					height='450'
+					allowFullScreen={true}
+					loading='lazy'
+					referrerPolicy='no-referrer-when-downgrade'></iframe>
+
+				<section className='flex flex-row my-36'>
+					{steps.map(step => {
+						return (
+							<article
+								key={step.id}
+								className='mr-5 overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm'>
+								<img
+									alt='Office'
+									src='https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+									className='h-56 w-full object-cover'
+								/>
+
+								<div className='p-4 sm:p-6'>
+									<a href='#'>
+										<h3 className='text-lg font-medium text-gray-900'>{step.title}</h3>
+									</a>
+
+									<p className='mt-2 line-clamp-3 text-sm/relaxed text-gray-500'>{step.description}</p>
+
+									<a href='#' className='group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600'>
+										Find out more
+										<span aria-hidden='true' className='block transition-all group-hover:ms-0.5 rtl:rotate-180'>
+											&rarr;
+										</span>
+									</a>
+								</div>
+							</article>
+						)
+					})}
+				</section>
+
+				<iframe
+					src='https://www.google.com/maps/embed?pb=!4v1683679674809!6m8!1m7!1sCAoSLEFGMVFpcFBpcXV6R0VBeDAxd2x2MTdsRVZfYTEyTTgtLWNGNmhWMXlZd1l6!2m2!1d26.39659659199523!2d-80.11636204720537!3f31.69!4f-20.049999999999997!5f0.5970117501821992'
+					width='100%'
+					height='450'
+					allowFullScreen={true}
+					loading='lazy'
+					referrerPolicy='no-referrer-when-downgrade'></iframe>
+			</main>
+
+			<Footer />
+		</>
+	)
 }
